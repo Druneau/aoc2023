0d5154f2259d6f6cc94be2a5ccef3a1694ef471a
Dominique Bruneau <druneau@gmail.com>
2023-12-12 12:32:50 -0500

    add day11 part1 and part2 solutions

--- day11/day11.py
+++ day11/day11.py
@@ -2,8 +2,23 @@ from itertools import combinations
 
 
 class CosmicImage:
-    def __init__(self, image):
+    def __init__(self, image, expansion_factor=2):
         self.image = image
+        self.expansion_factor = expansion_factor
+        self.h_expand_indexes = self.get_expand_row_indexes()
+        self.c_expand_indexes = self.get_expand_col_indexes()
+        self.find_galaxies()
+        self.calc_permutations()
+
+    def find_galaxies(self):
+        galaxy_locations = []
+        row_number = 0
+        for row in self.image:
+            for i in range(len(row)):
+                if row[i] == '#':
+                    galaxy_locations.append((row_number, i))
+            row_number += 1
+        self.galaxies = galaxy_locations
 
     def tag(self):
         galaxy_locations = []
@@ -28,6 +43,14 @@ class CosmicImage:
 
         return sum_distance
 
+    def calc_sum_distances_math(self):
+        sum_distance = 0
+        for permutation in self.permutations:
+            galaxy1, galaxy2 = permutation
+            sum_distance += self.calc_distance_math(galaxy1, galaxy2)
+
+        return sum_distance
+
     def calc_distance(self, galaxy1, galaxy2):
         _, galaxy1_location = galaxy1
         _, galaxy2_location = galaxy2
@@ -35,6 +58,49 @@ class CosmicImage:
         galaxy2_x, galaxy2_y = galaxy2_location
         distance = abs(galaxy1_x - galaxy2_x) + abs(galaxy1_y - galaxy2_y)
         return distance
+
+    def calc_distance_math(self, galaxy1, galaxy2):
+        galaxy1_r, galaxy1_c = galaxy1
+        galaxy2_r, galaxy2_c = galaxy2
+
+        r_n, c_n = self.get_number_expansions_between_galaxies(
+            galaxy1, galaxy2)
+
+        factor = max(0, self.expansion_factor - 1)
+
+        r_distance = abs(galaxy1_r - galaxy2_r)
+        c_distance = abs(galaxy1_c - galaxy2_c)
+
+        r_distance += r_n * factor
+        c_distance += c_n * factor
+
+        distance = r_distance + c_distance
+
+        # print('r:{}->{} n={};c:{}->{} n={}; d={}'.format(galaxy1_r,
+        #      galaxy2_r, r_n, galaxy1_c, galaxy2_c, c_n, distance))
+        return distance
+
+    def get_number_expansions_between_galaxies(self, galaxy1, galaxy2):
+        galaxy1_r, galaxy1_c = galaxy1
+        galaxy2_r, galaxy2_c = galaxy2
+
+        r_n = 0
+
+        low_r = min(galaxy1_r, galaxy2_r)
+        high_r = max(galaxy1_r, galaxy2_r)
+        for i in self.h_expand_indexes:
+            if low_r < i < high_r:
+                r_n += 1
+
+        c_n = 0
+
+        low_c = min(galaxy1_c, galaxy2_c)
+        high_c = max(galaxy1_c, galaxy2_c)
+        for i in self.v_expand_indexes:
+            if low_c < i < high_c:
+                c_n += 1
+
+        return r_n, c_n
 
     def expand(self):
         self.expand_cols()
@@ -45,10 +111,23 @@ class CosmicImage:
         for i in range(len(self.image)):
             row = self.image[i]
             if self.can_expand(row):
-                rows_to_expand.append(i)
+                for _ in range(self.expansion_factor - 1):
+                    rows_to_expand.append(i)
+
+        self.expand_rows = rows_to_expand
 
         for row_index in reversed(rows_to_expand):
             self.expand_row(row_index)
+
+    def get_expand_row_indexes(self):
+        rows_to_expand = []
+        for i in range(len(self.image)):
+            row = self.image[i]
+            if self.can_expand(row):
+                rows_to_expand.append(i)
+
+        self.h_expand_indexes = rows_to_expand
+        return rows_to_expand
 
     def expand_row(self, row_index):
         row = self.image[row_index]
@@ -59,7 +138,10 @@ class CosmicImage:
         for i in range(len(self.image[0])):
             col = self.extract_col(self.image, i)
             if self.can_expand(col):
-                cols_to_expand.append(i)
+                for _ in range(self.expansion_factor - 1):
+                    cols_to_expand.append(i)
+
+        self.expand_cols = cols_to_expand
 
         for col_index in reversed(cols_to_expand):
             self.expand_col(col_index)
@@ -67,6 +149,16 @@ class CosmicImage:
     def expand_col(self, col_index):
         for row in self.image:
             row.insert(col_index, '.')
+
+    def get_expand_col_indexes(self):
+        cols_to_expand = []
+        for i in range(len(self.image[0])):
+            col = self.extract_col(self.image, i)
+            if self.can_expand(col):
+                cols_to_expand.append(i)
+
+        self.v_expand_indexes = cols_to_expand
+        return cols_to_expand
 
     @staticmethod
     def can_expand(array):
@@ -83,13 +175,20 @@ def load(input):
     return image
 
 
-def part1(input='day11/input'):
+def part1(input='day11/input', expansion_factor=2):
     image = load(input)
-    cosmic_image = CosmicImage(image)
+    cosmic_image = CosmicImage(image, expansion_factor)
     cosmic_image.expand()
     cosmic_image.tag()
     cosmic_image.calc_permutations()
     return cosmic_image.calc_sum_distances()
+
+
+def part2(input='day11/input', expansion_factor=2):
+    image = load(input)
+    cosmic_image = CosmicImage(image, expansion_factor=expansion_factor)
+    cosmic_image.calc_permutations()
+    return cosmic_image.calc_sum_distances_math()
 
 
 def print_array(loop):
--- day11/test_day11.py
+++ day11/test_day11.py
@@ -1,8 +1,8 @@
 from day11 import (
     CosmicImage,
     part1,
+    part2,
     load,
-    print_array
 )
 
 import pytest
@@ -10,8 +10,107 @@ import pytest
 
 class TestPart1:
     def test_part1(self):
-        assert part1('day11/input_example') == 374
-        assert part1('day11/input') == 10173804
+        assert part1('day11/input_example', expansion_factor=1) == 292
+        assert part1('day11/input_example', expansion_factor=2) == 374
+        assert part1('day11/input_example', expansion_factor=10) == 1030
+        assert part1('day11/input_example', expansion_factor=100) == 8410
+        assert part1('day11/input', expansion_factor=2) == 10173804
+        assert part1('day11/input', expansion_factor=10) == 15248332
+
+
+class TestPart2:
+    def test_part2(self):
+        assert part2(expansion_factor=2) == 10173804
+        assert part2(expansion_factor=1000000) == 10173804
+
+
+class TestCosmicImageInit:
+    def test_cosmic_image_init(self):
+        image = load('day11/input_example')
+        cosmic_image = CosmicImage(image, expansion_factor=2)
+        assert cosmic_image.h_expand_indexes == [3, 7]
+        assert cosmic_image.v_expand_indexes == [2, 5, 8]
+
+        assert cosmic_image.image[2][0] == '#'
+        assert cosmic_image.image[0][3] == '#'
+
+
+class TestCosmicImageDistance:
+    def test_number_expansions_between_galaxies(self):
+        image = load('day11/input_simple')
+        cosmic_image = CosmicImage(image)
+        assert cosmic_image.get_number_expansions_between_galaxies(
+            (0, 0), (2, 2)) == (1, 1)
+
+        image = load('day11/input_example')
+        cosmic_image = CosmicImage(image)
+        assert cosmic_image.get_number_expansions_between_galaxies(
+            (0, 3), (9, 0)) == (2, 1)
+        assert cosmic_image.get_number_expansions_between_galaxies(
+            (2, 0), (6, 9)) == (1, 3)
+        assert cosmic_image.get_number_expansions_between_galaxies(
+            (0, 3), (1, 7)) == (0, 1)
+
+    def test_calc_distance_math_example(self):
+        image = load('day11/input_example')
+        cosmic_image = CosmicImage(image, expansion_factor=1)
+        assert cosmic_image.calc_distance_math((3, 0), (7, 1)) == 5
+        assert cosmic_image.calc_distance_math((3, 0), (0, 2)) == 5
+        assert cosmic_image.calc_distance_math((3, 0), (6, 4)) == 7
+        assert cosmic_image.calc_distance_math((3, 0), (1, 5)) == 7
+        assert cosmic_image.calc_distance_math((3, 0), (9, 6)) == 12
+        assert cosmic_image.calc_distance_math((3, 0), (7, 8)) == 12
+        assert cosmic_image.calc_distance_math((3, 0), (0, 9)) == 12
+        assert cosmic_image.calc_distance_math((3, 0), (4, 9)) == 10
+
+        cosmic_image = CosmicImage(image, expansion_factor=2)
+        assert cosmic_image.calc_distance_math((0, 3), (1, 7)) == 6
+
+        cosmic_image.expansion_factor = 10
+        assert cosmic_image.calc_distance_math((0, 3), (1, 7)) == 14
+        assert cosmic_image.calc_distance_math((0, 3), (9, 4)) == 28
+        assert cosmic_image.calc_distance_math((2, 0), (6, 9)) == 49
+
+        cosmic_image.expansion_factor = 100
+        assert cosmic_image.calc_distance_math((0, 3), (9, 4)) == 208
+        assert cosmic_image.calc_distance_math((2, 0), (6, 9)) == 409
+
+    def test_calc_sum_distance_math_simple(self):
+        image = load('day11/input_simple')
+        cosmic_image = CosmicImage(image, expansion_factor=1)
+        assert cosmic_image.calc_sum_distances_math() == 4
+
+        cosmic_image = CosmicImage(image, expansion_factor=2)
+        assert cosmic_image.calc_sum_distances_math() == 6
+
+        cosmic_image = CosmicImage(image, expansion_factor=10)
+        assert cosmic_image.calc_sum_distances_math() == 22
+
+        cosmic_image = CosmicImage(image, expansion_factor=100)
+        assert cosmic_image.calc_sum_distances_math() == 202
+
+    def test_calc_sum_distance_math_3_galaxies(self):
+        image = load('day11/input_3_galaxies')
+        cosmic_image = CosmicImage(image, expansion_factor=1)
+        assert cosmic_image.calc_sum_distances_math() == 8
+
+        cosmic_image = CosmicImage(image, expansion_factor=2)
+        assert cosmic_image.calc_sum_distances_math() == 12
+
+        cosmic_image = CosmicImage(image, expansion_factor=10)
+        assert cosmic_image.calc_sum_distances_math() == 44
+
+        cosmic_image = CosmicImage(image, expansion_factor=100)
+        assert cosmic_image.calc_sum_distances_math() == 404
+
+    def test_calc_sum_distance_math_example(self):
+        image = load('day11/input_example')
+        cosmic_image = CosmicImage(image, expansion_factor=1)
+        assert cosmic_image.calc_sum_distances_math() == 292
+
+        image = load('day11/input_example')
+        cosmic_image = CosmicImage(image, expansion_factor=2)
+        assert cosmic_image.calc_sum_distances_math() == 374
 
 
 class TestCosmicImageTagging:
@@ -241,3 +340,23 @@ class TestCosmicImageExpand:
         cosmic_image = CosmicImage(image)
         cosmic_image.expand()
         assert cosmic_image.image == image_expanded
+
+    def test_get_expand_indexes(self):
+        image = [
+            ['.', '.', '.'],
+            ['.', '#', '.'],
+            ['.', '.', '.'],
+        ]
+        cosmic_image = CosmicImage(image)
+        assert cosmic_image.get_expand_row_indexes() == [0, 2]
+        assert cosmic_image.get_expand_col_indexes() == [0, 2]
+
+    def test_get_expand_indexes(self):
+        image = [
+            ['.', '.', '.'],
+            ['.', '.', '.'],
+            ['.', '.', '.'],
+        ]
+        cosmic_image = CosmicImage(image)
+        assert cosmic_image.get_expand_row_indexes() == [0, 1, 2]
+        assert cosmic_image.get_expand_col_indexes() == [0, 1, 2]
