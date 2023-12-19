b8151e590b63280f8e2ebcc6ed83b69521a728b3
Dominique Bruneau <druneau@gmail.com>
2023-12-18 21:30:30 -0500

    working solutions for part1 and part2 day16

--- day16/day16.py
+++ day16/day16.py
@@ -2,6 +2,10 @@ from enum import Enum
 
 from typing import Tuple, List, Optional
 
+from DrawBeam import drawbeam
+
+import copy
+
 UP = (0, -1)
 DOWN = (0, 1)
 LEFT = (-1, 0)
@@ -112,19 +116,25 @@ class Beam():
         element = self.element
 
         if element == Elements.SPLITTER_VERTICAL and (self.direction == LEFT or self.direction == RIGHT):
-            if not self.contraption.has_element_split(self.location):
-                self.contraption.set_element_split(self.location)
+            if not self.contraption.has_element_split(location):
+                self.contraption.set_element_split(location)
                 beam_up = Beam(self.contraption, self.x, self.y, UP)
+                beam_up.breadcrumbs.add(location)
                 beam_down = Beam(self.contraption, self.x, self.y, DOWN)
+                beam_down.breadcrumbs.add(location)
                 self.is_alive = False
                 return [beam_up, beam_down]
+            self.is_alive = False
         elif element == Elements.SPLITTER_HORIZONTAL and (self.direction == UP or self.direction == DOWN):
-            if not self.contraption.has_element_split(self.location):
-                self.contraption.set_element_split(self.location)
+            if not self.contraption.has_element_split(location):
+                self.contraption.set_element_split(location)
                 beam_left = Beam(self.contraption, self.x, self.y, LEFT)
+                beam_left.breadcrumbs.add(location)
                 beam_right = Beam(self.contraption, self.x, self.y, RIGHT)
+                beam_right.breadcrumbs.add(location)
                 self.is_alive = False
                 return [beam_left, beam_right]
+            self.is_alive = False
         elif element == Elements.MIRROR_SLASH or element == Elements.MIRROR_BACKSLASH:
             if not self.contraption.has_bounced_directional(self.location, dir):
                 self.contraption.set_bounced_directional(self.location, dir)
@@ -133,39 +143,86 @@ class Beam():
                 self.is_alive = False
 
 
-def part1(input='day16/input.txt'):
+def load_file(input):
     _map = []
     with open(input, 'r') as file:
         _map = [line.strip() for line in file.readlines()]
+    return _map
 
-    contraption = Contraption(_map)
+
+def part1(input='day16/input.txt'):
+
+    contraption = Contraption(load_file(input))
 
     beams = [Beam(contraption, -1, 0, RIGHT)]
 
     while True:
-        new_beams = []
-        for beam in beams:
-            if beam.is_alive:
-                result = beam.step()
-                if result:
-                    new_beams.extend(result)
+        new_beams = [
+            result for beam in beams
+            if beam.is_alive
+            for result in beam.step() or []]
 
         beams.extend(new_beams)
 
-        if all(not beam.is_alive for beam in beams):
+        # drawbeam(contraption, beams)
+
+        if not any(beam.is_alive for beam in beams):
             break
 
-    merged_breadcrumbs = set()
-    for beam in beams:
-        merged_breadcrumbs.update(beam.breadcrumbs)
-
-    print(f"beam count: {len(beams)}")
+    merged_breadcrumbs = {
+        breadcrumb for beam in beams
+        for breadcrumb in beam.breadcrumbs}
 
     return len(merged_breadcrumbs)
 
 
-def part2():
-    pass
+def part2(input='day16/input.txt'):
+
+    contraption = Contraption(load_file(input))
+
+    root_beams = []
+
+    width = contraption.width
+    height = contraption.height
+
+    # Top and bottom edges
+    for col in range(width):
+        beam_down = Beam(copy.deepcopy(contraption), col, -1, DOWN)
+        beam_up = Beam(copy.deepcopy(contraption), col, height, UP)
+        root_beams.extend([beam_down, beam_up])
+
+    # Left and right edges
+    for row in range(height):
+        beam_right = Beam(copy.deepcopy(contraption), -1, row, RIGHT)
+        beam_left = Beam(copy.deepcopy(contraption), width, row, LEFT)
+        root_beams.extend([beam_right, beam_left])
+
+    max_energized = 0
+
+    for root_beam in root_beams:
+        beams = [root_beam]
+
+        while True:
+
+            new_beams = [
+                result for beam in beams
+                if beam.is_alive
+                for result in beam.step() or []]
+
+            beams.extend(new_beams)
+
+            # drawbeam(contraption, beams)
+
+            if not any(beam.is_alive for beam in beams):
+                break
+
+        merged_breadcrumbs = set()
+        for beam in beams:
+            merged_breadcrumbs.update(beam.breadcrumbs)
+
+        max_energized = max(max_energized, len(merged_breadcrumbs))
+
+    return max_energized
 
 
 if __name__ == '__main__':
--- day16/test_day16.py
+++ day16/test_day16.py
@@ -25,6 +25,14 @@ def contrap_example():
     return day16.Contraption(input)
 
 
+@pytest.fixture
+def contrap_loop():
+    input = ['/.\\',
+             '...',
+             '\\./']
+    return day16.Contraption(input)
+
+
 class TestContraption:
 
     def test_contraption_init(self, contrap_empty, contrap_example):
@@ -61,15 +69,22 @@ class TestContraption:
         assert contrap_example.has_bounced_directional((4, 1), UP) is False
 
 
-class TestBeam():
+@pytest.fixture
+def beam_empty(contrap_empty):
+    return day16.Beam(contrap_empty, -1, 0, RIGHT)
 
-    @pytest.fixture
-    def beam_empty(self, contrap_empty):
-        return day16.Beam(contrap_empty, -1, 0, RIGHT)
 
-    @pytest.fixture
-    def beam_example(self, contrap_example):
-        return day16.Beam(contrap_example, -1, 0, RIGHT)
+@pytest.fixture
+def beam_example(contrap_example):
+    return day16.Beam(contrap_example, -1, 0, RIGHT)
+
+
+@pytest.fixture
+def beam_loop(contrap_loop):
+    return day16.Beam(contrap_loop, 1, 0, RIGHT)
+
+
+class TestBeamFunctions():
 
     def test_bounce(self):
         assert day16.Beam.bounce(Elements.MIRROR_SLASH, UP) == RIGHT
@@ -107,7 +122,10 @@ class TestBeam():
         assert beam_empty.location == (2, 0)
         assert beam_empty.is_alive is True
 
-    def test_beam_step_into_spillter_vertical(self, beam_example):
+
+class TestBeamLogic:
+
+    def test_beam_step_into_spliter_vertical(self, beam_example):
         beam_example.step()
         assert beam_example.element == Elements.EMPTY_SPACE
 
@@ -127,7 +145,43 @@ class TestBeam():
         assert beam_down.location == (1, 1)
         assert beam_down.is_alive is True
 
-    def test_beam_step_into_splitter_horizontal(self, beam_example):
+    def test_beam_step_into_splitter_horizontal_towards_up(self, beam_example):
+        beam_example.x = 2
+        beam_example.y = 2
+        beam_example.direction = UP
+        assert beam_example.element == Elements.EMPTY_SPACE
+        beam_left, beam_right = beam_example.step()
+        assert beam_example.element == Elements.SPLITTER_HORIZONTAL
+        assert beam_example.location == (2, 1)
+        assert beam_example.is_alive is False
+
+        assert beam_right.breadcrumbs == {(2, 1)}
+        assert beam_right.direction == RIGHT
+        result = beam_right.step()
+        assert beam_right.location == (3, 1)
+
+        assert beam_left.breadcrumbs == {(2, 1)}
+        assert beam_left.direction == LEFT
+        result = beam_left.step()
+        assert beam_left.location == (1, 1)
+
+    def test_beam_step_into_spliter_horizontal_towards_right(self, beam_example):
+        beam_example.x = 1
+        beam_example.y = 1
+        beam_example.direction = RIGHT
+        assert beam_example.element == Elements.EMPTY_SPACE
+        result = beam_example.step()
+        assert result is None
+        assert beam_example.location == (2, 1)
+        assert beam_example.element == Elements.SPLITTER_HORIZONTAL
+        result = beam_example.step()
+        assert result is None
+        assert beam_example.location == (3, 1)
+        assert beam_example.element == Elements.EMPTY_SPACE
+
+        assert beam_example.breadcrumbs == {(2, 1), (3, 1)}
+
+    def test_beam_step_into_spliter_horizontal_towards_down(self, beam_example):
         beam_example.x = 1
         beam_example.y = 6
         beam_example.direction = DOWN
@@ -138,24 +192,165 @@ class TestBeam():
 
         beam_left.step()
         assert beam_left.location == (0, 7)
+        assert beam_left.direction == LEFT
         beam_left.step()
         assert beam_left.is_alive is False
+        beam_left.step()
+        beam_left.step()
+        beam_left.step()
+        assert beam_left.breadcrumbs == {(1, 7), (0, 7)}
 
+        assert beam_right.direction == RIGHT
         beam_right.step()
         assert beam_right.location == (2, 7)
         beam_right.step()
         assert beam_right.element == Elements.SPLITTER_HORIZONTAL
         beam_right.step()
         assert beam_right.is_alive is True
+        assert beam_right.breadcrumbs == {(1, 7), (2, 7), (3, 7), (4, 7)}
 
-    def test_beam_step_into_mirror_slash(self, beam_example):
+    def test_beam_step_into_spliter_vertical_towards_right(self, beam_example):
+        beam_example.x = 7
+        beam_example.y = 3
+        beam_example.direction = RIGHT
+        assert beam_example.element == Elements.EMPTY_SPACE
+        beam_up, beam_down = beam_example.step()
+        assert beam_example.breadcrumbs == {(8, 3)}
+        assert beam_example.is_alive is False
+
+        assert beam_up.direction == UP
+        result = beam_up.step()
+        assert result is None
+        assert beam_up.location == (8, 2)
+        assert beam_up.element == Elements.EMPTY_SPACE
+
+        assert beam_down.direction == DOWN
+        result = beam_down.step()
+        assert result is None
+        assert beam_down.location == (8, 4)
+        assert beam_down.element == Elements.EMPTY_SPACE
+
+    def test_beam_step_into_mirror_slash_towards_right(self, beam_example):
         beam_example.x = 3
         beam_example.y = 6
-        ret_value = beam_example.step()
-        assert ret_value is None
+        beam_example.direction = RIGHT
+        result = beam_example.step()
+        assert result is None
+        assert beam_example.location == (4, 6)
         assert beam_example.direction == UP
+        assert beam_example.element == Elements.MIRROR_SLASH
+        result = beam_example.step()
+        assert result is None
+        assert beam_example.location == (4, 5)
+        assert beam_example.is_alive is True
+
+    def test_beam_step_into_mirror_slash_towards_down(self, beam_example):
+        beam_example.x = 4
+        beam_example.y = 5
+        beam_example.direction = DOWN
+        result = beam_example.step()
+        assert result is None
+        assert beam_example.location == (4, 6)
+        assert beam_example.direction == LEFT
+        assert beam_example.element == Elements.MIRROR_SLASH
+        result = beam_example.step()
+        assert result is None
+        assert beam_example.location == (3, 6)
+        assert beam_example.is_alive is True
+
+    def test_beam_step_into_mirror_slash_towards_left(self, beam_example):
+        beam_example.x = 5
+        beam_example.y = 6
+        beam_example.direction = LEFT
+        result = beam_example.step()
+        assert result is None
+        assert beam_example.location == (4, 6)
+        assert beam_example.direction == DOWN
+        assert beam_example.element == Elements.MIRROR_SLASH
+        result = beam_example.step()
+        assert result is None
+        assert beam_example.location == (4, 7)
+        assert beam_example.is_alive is True
+
+    def test_beam_step_into_mirror_slash_towards_up(self, beam_example):
+        beam_example.x = 4
+        beam_example.y = 7
+        beam_example.direction = UP
+        result = beam_example.step()
+        assert result is None
+        assert beam_example.location == (4, 6)
+        assert beam_example.direction == RIGHT
+        assert beam_example.element == Elements.MIRROR_SLASH
+        result = beam_example.step()
+        assert result is None
+        assert beam_example.location == (5, 6)
+        assert beam_example.is_alive is True
+
+    def test_beam_step_into_mirror_backslash_towards_right(self, beam_example):
+        # from the right
+        beam_example.x = 3
+        beam_example.y = 1
+        result = beam_example.step()
+        assert beam_example.location == (4, 1)
+        assert result is None
+        assert beam_example.direction == DOWN
+        result = beam_example.step()
+        assert result is None
+        assert beam_example.location == (4, 2)
+
+    def test_beam_step_into_mirror_backslash_towards_up(self, beam_example):
+        # from the right
+        beam_example.x = 4
+        beam_example.y = 2
+        beam_example.direction = UP
+        result = beam_example.step()
+        assert beam_example.location == (4, 1)
+        assert result is None
+        assert beam_example.direction == LEFT
+        result = beam_example.step()
+        assert result is None
+        assert beam_example.location == (3, 1)
+
+    def test_beam_step_into_mirror_backslash_towards_down(self, beam_example):
+        # from the right
+        beam_example.x = 4
+        beam_example.y = 0
+        beam_example.direction = DOWN
+        result = beam_example.step()
+        assert beam_example.location == (4, 1)
+        assert result is None
+        assert beam_example.direction == RIGHT
+        result = beam_example.step()
+        assert result is None
+        assert beam_example.location == (5, 1)
+
+    def test_beam_step_into_mirror_backslash_towards_left(self, beam_example):
+        # from the right
+        beam_example.x = 5
+        beam_example.y = 1
+        beam_example.direction = LEFT
+        result = beam_example.step()
+        assert beam_example.location == (4, 1)
+        assert result is None
+        assert beam_example.direction == UP
+        result = beam_example.step()
+        assert result is None
+        assert beam_example.location == (4, 0)
+
+    def test_beam_loop_stops(self, beam_loop):
+        while beam_loop.is_alive:
+            result = beam_loop.step()
+            assert result is None
+        assert beam_loop.is_alive is False
+        assert len(beam_loop.breadcrumbs) == 8
 
 
 def test_part1():
     assert day16.part1('day16/input_example.txt') == 46
-    assert day16.part1() == 8146
+    # assert day16.part1('day16/input_ben.txt') == 7632
+    # assert day16.part1() == 8146
+
+
+def test_part2():
+    assert day16.part2('day16/input_example.txt') == 51
+    # assert day16.part2() == 8358
